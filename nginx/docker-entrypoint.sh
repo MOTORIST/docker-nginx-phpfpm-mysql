ca824e38949382ec1a33ef2e60d1c05b89784ffb
MOTORIST <webartmotor@gmail.com>
2019-04-07 02:36:32 +0300

    refactor: add certificate to nginx config

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -9,6 +9,7 @@ if [[ $NGINX_SSL_CERT == "yes" ]]; then
     mkdir -p /etc/nginx/include;
     envsubst '$$NGINX_ROOT_FRONTEND' < /etc/nginx/ssl.redirect.template > /etc/nginx/include/ssl.redirect.conf
     cat /etc/nginx/ssl_config.template > /etc/nginx/include/ssl_config.conf
+    cat /etc/nginx/ssl_certificate.template > /etc/nginx/include/ssl_certificate.conf
 fi
 
 envsubst '$$NGINX_ROOT_FRONTEND $$NGINX_ROOT_BACKEND $$NGINX_SERVER_NAME_FRONTEND $$NGINX_SERVER_NAME_BACKEND $$NGINX_PORT' < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
